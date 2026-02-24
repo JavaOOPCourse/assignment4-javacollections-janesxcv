a8fcbe0de6ac2cd91b17ca89dff55bccb0369a0d
aiperi <kydykbekovaajperi@gmail.com>
2026-02-24 15:38:07 +0600

    Add appointment initialization and cancellation methods
    Initialized appointments with three normal and one urgent appointment.

--- src/service/AppointmentService.java
+++ src/service/AppointmentService.java
@@ -10,17 +10,42 @@ public class AppointmentService {
     public void initializeAppointments() {
         // TODO: Add 3 normal appointments
         // TODO: Add 1 urgent appointment at the beginning
+        appointments.add("appointment 1");
+        appointments.add("appointement 2");
+        appointments.add("appointment 3");
+
+        appointments.addFirst("urgent appointment");
+
+        System.out.println("appointments initialized");
     }
 
     public void cancelLast() {
         // TODO: Remove last appointment
+        if (!appointments.isEmpty()) {
+            String cancelled = appointments.removeLast();
+            System.out.println("removed appointment :"+ cancelled);
+        } else {
+            System.out.println("no appointments found");
+        }
+
     }
 
     public void showFirstAndLast() {
         // TODO: Print first and last appointment
+        if (!appointments.isEmpty()) {
+        String first = appointments.peekFirst();
+        String last = appointments.peekLast();
+            System.out.println("first :" +first);
+            System.out.println("last :"+ last); }
+
     }
 
     public void printAppointments() {
         // TODO: Traverse using Iterator
+        Iterator<String> it = appointments.iterator();
+
+        while (it.hasNext()) {
+            System.out.println("--" + it.next());
+        }
     }
 }
