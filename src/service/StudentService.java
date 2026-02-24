c127175dfc8536ef24fb82e2bd592c73c3506aba
aiperi <kydykbekovaajperi@gmail.com>
2026-02-24 15:39:30 +0600

    Initialize student list and implement GPA methods

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -9,22 +9,46 @@ public class StudentService {
     private ArrayList<Student> students = new ArrayList<>();
 
     public void initializeStudents() {
-        // TODO: Add at least 5 students
+        students.add(new Student("Aiperi", 113, 4.0));
+        students.add(new Student("Mirana", 114, 3.0));
+        students.add(new Student("Nazik", 115, 2.0));
+        students.add(new Student("Aibiyke", 116, 3.9));
+        students.add(new Student("Dilya", 117, 2.0));
+        System.out.println("Student list was initialized");
     }
 
     public void removeLowGPA() {
-        // TODO: Remove students with GPA < 2.0
+        students.removeIf(s-> s.getGpa() < 2.0);
     }
 
     public void findHighestGPA() {
         // TODO: Find and print student with highest GPA
+        Student TopStudent = students.get(0);
+        for (Student s : students) {
+            if (s.getGpa() > TopStudent.getGpa()) {
+                TopStudent = s;
+            }
+        }
+        System.out.println("Student with highest gpa :" +TopStudent);
     }
 
     public void insertAtIndex() {
         // TODO: Insert new student at index 2
+        if (students.size() >= 2) {
+            students.add(2, new Student("Kasym", 118, 3.5));
+            System.out.println("Kasym was added to students");
+        }
     }
 
-    public void printStudents() {
+    public void  printStudents() {
         // TODO: Print using Iterator
+        System.out.println("-----Current Students------");
+
+        Iterator<Student> iterator = students.iterator();
+
+        while (iterator.hasNext()) {
+            Student s = iterator.next();
+            System.out.println(s);
+        }
     }
 }
